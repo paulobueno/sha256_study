ec13f07c62032839dc0c00adcaed43b2205994a4
Paulo Bueno <paulob.bruno@gmail.com>
2021-01-20 00:24:14 -0300

    added 512 feature

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-word = 'Hello world'
+word = 'Hello world abcde abcdeyutioiuioa a a a a asd asdasd asd asd asd asd asd asd aaaaaaaa'
 b_word = bytearray(word,'utf-8')
-print(b_word)
+b_word_bitlen = len(b_word)*8
+b_word.append(int('10000000',2))
+while len(b_word)%(64-8) != 0:
+    b_word.append(0)
+b_word_len_bytearray = (b_word_bitlen).to_bytes(8, byteorder='big')
+b_word.extend(b_word_len_bytearray)
+int_list = [n for n in b_word]
+print(int_list)
