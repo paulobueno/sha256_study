3378ecfcbe32902c9bfd5e78b457d11443c63a59
Paulo Bueno <paulob.bruno@gmail.com>
2021-02-07 00:08:38 -0300

    done step 5 of 8

--- main.py
+++ main.py
@@ -58,28 +58,42 @@ def rightshift_str(string, qty):
     return '0'*qty + string[:-qty]
 
 def xor(list):
-	element = list[0]
-	for i in list[1:]:
-		_element = ''
-		for k,v in enumerate(i):
-			if v != element[k]:
-				_element += '1'
-			else:
-				_element += '0'
-		element = _element
-	return element
-	
+    element = list[0]
+    for i in list[1:]:
+        _element = ''
+        for k,v in enumerate(i):
+            if v != element[k]:
+                _element += '1'
+            else:
+                _element += '0'
+        element = _element
+    return element
+
+def sum_bin_str(list_bin_str,str_lenght=8):
+    sum_result = 0
+    for bin_str in list_bin_str:
+        sum_result += int(bin_str,2)
+    output_config = '{0:0' + str(str_lenght) + 'b}'
+    result = output_config.format(sum_result) 
+    return result[-str_lenght:] 
+
 def modify_chunks(chunk_list):
-	for k,v in enumerate(chunk_list[16:], start=16):
-		s0_a = rightrotate_str(chunk_list[k-15],7)
-		s0_b = rightrotate_str(chunk_list[k-15],18)
-		s0_c = rightshift_str(chunk_list[k-15],3)
-		s0 = xor([s0_a,s0_b,s0_c])
-	return s0_b
+    for k,v in enumerate(chunk_list[16:], start=16):
+        s0_a = rightrotate_str(chunk_list[k-15],7)
+        s0_b = rightrotate_str(chunk_list[k-15],18)
+        s0_c = rightshift_str(chunk_list[k-15],3)
+        s0 = xor([s0_a,s0_b,s0_c])
+        s1_a = rightrotate_str(chunk_list[k-2],17)
+        s1_b = rightrotate_str(chunk_list[k-2],19)
+        s1_c = rightshift_str(chunk_list[k-2],10)
+        s1 = xor([s1_a,s1_b,s1_c])
+        chunk_list[k] = sum_bin_str([chunk_list[k-16],s0,chunk_list[k-7],s1],32)
+    return chunk_list
 
 if __name__=='__main__':
     word = 'hello world'
     chunk = gen_chunk(word)
-    mschedule = gen_message_schedule(chunk)
-    for i in mschedule:
+    chunks = gen_message_schedule(chunk)
+    chunks = modify_chunks(chunks)
+    for i in chunks:
         print(i)
