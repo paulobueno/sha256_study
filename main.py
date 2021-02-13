8f7e2e77e60f8c0e6266cb22e9ff3b838f53a837
Paulo Bueno Bruno <paulob.bruno@gmail.com>
2021-02-12 23:51:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -25,22 +25,23 @@ def get_prime_numbers(qty):
             prime_list.append(n)
     return prime_list
 
-def get_hash_values():
+def get_hash_values(size=32):
     h_hash_values = {}
     for k,prime in enumerate(get_prime_numbers(8),1):
         prime_sqrt = math.sqrt(prime)
         fracional, whole = math.modf(prime_sqrt)
         bin_fracional = int(fracional*(1<<32))
-        h_hash_values['h'+str(k)] = hex(bin_fracional)
+        h_hash_values['h'+str(k)] = format(bin_fracional,f'0>{size}b')
     return h_hash_values
 
-def get_round_constants():
+def get_round_constants(size=32):
     constants_list = []
     for k,prime in enumerate(get_prime_numbers(64),1):
         prime_sqrt = prime**(1/3)
         fracional, whole = math.modf(prime_sqrt)
         bin_fracional = int(fracional*(1<<32))
-        constants_list.append(hex(bin_fracional))
+        bin_fracional = format(bin_fracional,f'0>{size}b')
+        constants_list.append(bin_fracional)
     return constants_list
 
 def gen_message_schedule(bin_str):
@@ -68,6 +69,27 @@ def xor(list):
                 _element += '0'
         element = _element
     return element
+    
+def and_bin(list):
+    element = list[0]
+    for i in list[1:]:
+        _element = ''
+        for k,v in enumerate(i):
+            if v == '1' and element[k] == '1':
+                _element += '1'
+            else:
+                _element += '0'
+        element = _element
+    return element
+    
+def not_bin(element):
+	_element = ''
+	for i in element:
+		if i == '1':
+			_element += '0'
+		else:
+			_element += '1'
+	return _element
 
 def sum_bin_str(list_bin_str,str_lenght=8):
     sum_result = 0
@@ -96,7 +118,57 @@ def step_5(word):
     chunks = modify_chunks(chunks)
     return chunks
 
+def step_6(word):
+	k = get_round_constants()
+	w = step_5(word)
+	hash_values = get_hash_values()
+	a = hash_values['h1']
+	b = hash_values['h2']
+	c = hash_values['h3']
+	d = hash_values['h4']
+	e = hash_values['h5']
+	f = hash_values['h6']
+	g = hash_values['h7']
+	h = hash_values['h8']
+	
+	for i in range(64):
+		s1_a = rightrotate_str(e,6)
+		s1_b = rightrotate_str(e,11)
+		s1_c = rightrotate_str(e,25)
+		s1 = xor([s1_a,s1_b,s1_c])
+		ch_a = and_bin([e,f])
+		ch_b = not_bin(e)
+		ch_c = and_bin([ch_b,g])
+		ch = xor([ch_a,ch_c])
+		temp1 = sum_bin_str([h,s1,ch,k[i],w[i]],32)
+		s0_a = rightrotate_str(a,2)
+		s0_b = rightrotate_str(a,13)
+		s0_c = rightrotate_str(a,22)
+		s0 = xor([s0_a,s0_b,s0_c])
+		maj = xor([and_bin([a,b]),and_bin([a,c]),and_bin([b,c])])
+		temp2 = sum_bin_str([s0,maj],32)
+		h = g
+		g = f
+		e = sum_bin_str([d,temp1],32)
+		d = c
+		c = b
+		b = a
+		a = sum_bin_str([temp1,temp2],32)
+		
+	h1 = sum_bin_str([hash_values['h1'],a],32)
+	h2 = sum_bin_str([hash_values['h2'],b],32)
+	h3 = sum_bin_str([hash_values['h3'],c],32)
+	h4 = sum_bin_str([hash_values['h4'],d],32)
+	h5 = sum_bin_str([hash_values['h5'],e],32)
+	h6 = sum_bin_str([hash_values['h6'],f],32)
+	h7 = sum_bin_str([hash_values['h7'],g],32)
+	h8 = sum_bin_str([hash_values['h8'],h],32)
+	
+	final = h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8
+	final = str(hex(int(final,2)))[2:]
+	return final.upper()
+	
+
 if __name__=='__main__':
     word = 'hello world'
-    for i in step_5(word):
-        print(i)
+    print(step_6(word))
